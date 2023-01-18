25872dc1552569d246650daf72bc70bd6a31c108
ahmedHamed993 <ronaldo_ahmed48@yahoo.com>
2023-01-18 10:59:43 +0200

    initail upload

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import React,{useState, useEffect} from 'react';
+import '../styles/globals.scss';
+import "tailwindcss/tailwind.css";
+import {Layout} from "../components";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+import {PostCard, PostWidget, Categories, Header} from "../components";
+import { getPosts } from '../services';
+import FeaturedPosts from '../sections/FeaturedPosts';
+
+export default function Home({ posts }) {
+  console.log(posts);
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+
+      <Head>
+        <title>Ql Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <FeaturedPosts />
+
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        { /* posts container  */}
+        <div className='lg:col-span-8 col-span-1'>
+          { posts.map( post=> <PostCard post={post.node} key={post.title} /> ) }
+        </div>
+
+        {/* right widgets  */}
+        <div className="lg:col-span-4 col-span-1">
+          <div className='lg:sticky relative top-8'>
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props : { posts }
+  }
+}
